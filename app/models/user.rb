47c1d7bd49f6cc3a2065e973b634f0d0e9b53791
chrisbradshaw <christopherbradshaw11@gmail.com>
2017-11-21 15:47:15 -0500

    add user subscriptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   	MyMailer.new_user(self).deliver
   end
 
-  def self.find_for_google_oauth2(access_token, signed_in_resourse=nil)
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
   	data = access_token.info
   	user = User.where(:provider => access_token.provider, :uid => access_token.uid).first
 
